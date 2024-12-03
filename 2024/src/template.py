421563a34b5ac6e3b5c938dd6c624956394d9768
abidkyo <kyotasuna95@gmail.com>
2024-12-03 16:04:00 +0100

    2024 ENH template

---
+++ 2024/src/template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+"""
+Template for AOC Solution.
+"""
+
+from aoc import AOCSolver
+
+# ------------------------------------------------------------------------------
+
+
+class Day00Solver(AOCSolver):
+    def __init__(self):
+        super().__init__()
+
+        self.day = 0
+        self.expected_test = 0, 0
+        self.expected = 0, 0
+
+    def solve(self, test: bool = False) -> tuple:
+        p1, p2 = 0, 0
+
+        return p1, p2
+
+
+# ------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    solver = Day00Solver()
+    # solver.run()
+
+    test = 1
+    puzzle = solver.read_input(test).splitlines()
+
+    G = {}
+    P = complex
+    for y, r in enumerate(puzzle):
+        for x, c in enumerate(r):
+            G[P(x, y)] = c
+
+
+# ------------------------------------------------------------------------------
