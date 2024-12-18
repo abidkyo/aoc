72ce9d923ecf9b4b5fd539dbc15e3e9dde2c411a
abidkyo <kyotasuna95@gmail.com>
2024-12-18 07:30:17 +0100

    2024 ENH day 18

---
+++ 2024/src/day18.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+"""
+Shortest-Path with Binary-Search.
+
+python3: 7.2s
+"""
+
+from aoc import DIRC, AOCSolver, integers
+
+# ------------------------------------------------------------------------------
+
+
+def bfs(W, S, E):
+    queue = [(S, 0)]
+    seen = set([S])
+
+    for p, s in queue:
+        if p == E:
+            return s
+        for d in (DIRC[d] for d in "urdl"):
+            np = p + d
+            if (
+                np in seen
+                or np in W
+                or not (0 <= np.real <= E.real and 0 <= np.imag <= E.imag)
+            ):
+                continue
+            seen.add(np)
+            queue.append((np, s + 1))
+
+    return 0
+
+
+class Day18Solver(AOCSolver):
+    def __init__(self):
+        super().__init__()
+
+        self.day = 18
+        self.expected_test = 22, (6 + 1j)
+        self.expected = 384, (36 + 10j)
+
+    def solve(self, test: bool = False) -> tuple:
+        puzzle = solver.read_input(test).splitlines()
+
+        P = complex
+        W = [P(*integers(p)) for p in puzzle]
+
+        S = P(0, 0)
+        if test:
+            E = P(6, 6)
+            L = 12
+        else:
+            E = P(70, 70)
+            L = 1024
+
+        w = set(W[:L])
+        p1 = bfs(w, S, E)
+
+        for i in range(L, len(W)):
+            w.add(W[i])
+            if bfs(w, S, E) == 0:
+                p2 = W[i]
+                break
+
+        return p1, p2
+
+
+# ------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    solver = Day18Solver()
+    solver.run()
+
+
+# ------------------------------------------------------------------------------
