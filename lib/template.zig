2205d6041105977b2c6019266d23f03d1f0d37cb
abidkyo <kyotasuna95@gmail.com>
2025-12-07 14:30:46 +0100

    template MNT minor changes
    - use usize as result type
    - default to 0, not undefined

--- lib/template.zig
+++ lib/template.zig
@@ -7,8 +7,8 @@ const aoc = @import("aoc");
 const pos = @import("position");
 
 pub const Result = struct {
-    p1: u32,
-    p2: u32,
+    p1: usize,
+    p2: usize,
 
     pub fn format(self: @This(), writer: *std.Io.Writer) std.Io.Writer.Error!void {
         return writer.print("p1 = {d}, p2 = {d}", .{ self.p1, self.p2 });
@@ -18,7 +18,7 @@ pub const Result = struct {
 pub fn solve(allocator: Allocator, data: []const u8, test_run: bool) !Result {
     _ = test_run;
 
-    var result: Result = .{ .p1 = undefined, .p2 = undefined };
+    var result: Result = .{ .p1 = 0, .p2 = 0 };
 
     const lines = try aoc.splitlines(allocator, data);
     defer allocator.free(lines);
@@ -43,8 +43,8 @@ pub fn main() !void {
         "{{year}}",
         "{{day}}",
         solve,
-        Result{ .p1 = undefined, .p2 = undefined },
-        Result{ .p1 = undefined, .p2 = undefined },
+        Result{ .p1 = 0, .p2 = 0 },
+        Result{ .p1 = 0, .p2 = 0 },
     );
 
     solver.info();
